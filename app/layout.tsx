0f2d03339756f9020d6717ee6a06e1a57c4c1f08
Sumit Rathod <sbrathod22kl@gmail.com>
2026-05-27 13:23:03 +0530

    posthog error fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,30 +34,30 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} ${geist.variable} min-h-screen antialiased`}
       >
-        <PostHogProvider>
-          <Suspense fallback={null}>
+        <Suspense fallback={null}>
+          <PostHogProvider>
             <PostHogPageView />
-          </Suspense>
-          <Navbar />
-          <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-            <LightRays
-              raysOrigin="top-center-offset"
-              raysColor="#5dfeca"
-              raysSpeed={0.5}
-              lightSpread={0.9}
-              rayLength={1.4}
-              followMouse={true}
-              mouseInfluence={0.1}
-              noiseAmount={0.0}
-              distortion={0.01}
-              className="custom-rays"
-              pulsating={false}
-              fadeDistance={1}
-              saturation={1}
-            />
-          </div>
-          <main>{children}</main>
-        </PostHogProvider>
+            <Navbar />
+            <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+              <LightRays
+                raysOrigin="top-center-offset"
+                raysColor="#5dfeca"
+                raysSpeed={0.5}
+                lightSpread={0.9}
+                rayLength={1.4}
+                followMouse={true}
+                mouseInfluence={0.1}
+                noiseAmount={0.0}
+                distortion={0.01}
+                className="custom-rays"
+                pulsating={false}
+                fadeDistance={1}
+                saturation={1}
+              />
+            </div>
+            <main>{children}</main>
+          </PostHogProvider>
+        </Suspense>
       </body>
     </html>
   );
