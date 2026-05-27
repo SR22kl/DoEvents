ba80e80b7f43361712d52b7bcec8b9ffa0eb0321
Sumit Rathod <sbrathod22kl@gmail.com>
2026-05-27 15:04:49 +0530

    EventsDetails component added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,15 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
 // import { events } from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Home = async () => {
-  const response = await fetch(`${BASE_URL}/api/events`, {
-    cache: "no-store",
-  });
+  "use cache";
+  cacheLife("hours");
+  const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
 
   return (
